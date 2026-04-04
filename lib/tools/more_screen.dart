61596d07299a50db3b83e65ec497471aa229a6e6
y-ploni <7353755@gmail.com>
2026-04-04 22:13:47 +0300

    מערכת התוספים: שלבים 0, 1

--- lib/tools/more_screen.dart
+++ lib/tools/more_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:otzaria/navigation/bloc/navigation_bloc.dart';
 import 'package:otzaria/navigation/bloc/navigation_state.dart';
+import 'package:otzaria/widgets/custom_ui_components.dart';
 import 'package:otzaria/tools/measurement_converter/measurement_converter_screen.dart';
 import 'package:otzaria/tools/gematria/gematria_search_screen.dart';
 import 'package:otzaria/tools/dictionary/dictionary_screen.dart';
@@ -10,6 +12,84 @@ import 'package:otzaria/tools/shamor_zachor/shamor_zachor.dart';
 import 'package:otzaria/tools/calendar/ulits/calendar_widget.dart';
 import 'package:otzaria/tools/calendar/ulits/calendar_cubit.dart';
 import 'package:otzaria/personal_notes/view/personal_notes_screen.dart';
+import 'package:otzaria/plugins/bloc/plugin_system_bloc.dart';
+import 'package:otzaria/plugins/bloc/plugin_system_state.dart';
+import 'package:otzaria/plugins/bloc/plugin_system_event.dart';
+import 'package:otzaria/plugins/view/plugin_side_panel.dart';
+import 'package:otzaria/plugins/view/plugin_tab_page.dart';
+import 'package:otzaria/plugins/view/plugin_test_screen.dart';
+import 'package:otzaria/plugins/models/installed_plugin.dart';
+
+abstract class ToolDescriptor {
+  final String toolId;
+  final String label;
+  final int order;
+  const ToolDescriptor({required this.toolId, required this.label, required this.order});
+  Widget buildTab(BuildContext context);
+  Widget buildPage(BuildContext context);
+}
+
+class BuiltInToolDescriptor extends ToolDescriptor {
+  final IconData? icon;
+  final String? imageIcon;
+  final Widget Function() pageBuilder;
+
+  const BuiltInToolDescriptor({
+    required super.toolId,
+    required super.label,
+    required super.order,
+    this.icon,
+    this.imageIcon,
+    required this.pageBuilder,
+  });
+
+  @override
+  Widget buildTab(BuildContext context) {
+    if (imageIcon != null) {
+      return SizedBox(
+        width: 100,
+        child: Tab(text: label, icon: ImageIcon(AssetImage(imageIcon!), size: 20)),
+      );
+    }
+    return SizedBox(
+      width: 100,
+      child: Tab(text: label, icon: Icon(icon, size: 20)),
+    );
+  }
+
+  @override
+  Widget buildPage(BuildContext context) => pageBuilder();
+}
+
+class PluginToolDescriptor extends ToolDescriptor {
+  final InstalledPlugin plugin;
+  final bool isTransient;
+  PluginToolDescriptor({required this.plugin, this.isTransient = false}) 
+      : super(toolId: plugin.pluginId, label: plugin.manifest.toolTabTitle, order: plugin.manifest.toolTabOrder);
+
+  @override
+  Widget buildTab(BuildContext context) {
+    return SizedBox(
+      width: 100,
+      child: Tab(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(FluentIcons.puzzle_piece_24_regular, size: 16),
+            const SizedBox(width: 8),
+            Flexible(child: Text(label, overflow: TextOverflow.ellipsis, style: isTransient ? const TextStyle(fontStyle: FontStyle.italic) : null)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildPage(BuildContext context) => PluginTabPage(
+    key: ValueKey(plugin.pluginId),
+    plugin: plugin,
+  );
+}
 
 class MoreScreen extends StatefulWidget {
   const MoreScreen({super.key});
@@ -18,151 +98,193 @@ class MoreScreen extends StatefulWidget {
   MoreScreenState createState() => MoreScreenState();
 }
 
-/// מצב מסך "עוד".
-///
-/// אחראי על מעבר בין לשוניות הכלים ועל החזרת פוקוס יזומה ללוח השנה
-/// כאשר חוזרים אליו ממסך אחר או מלשונית אחרת.
 class MoreScreenState extends State<MoreScreen>
     with TickerProviderStateMixin, AutomaticKeepAliveClientMixin {
   static const int _calendarFocusRetryCount = 6;
-  late final TabController _tabController;
-  final GlobalKey<CalendarWidgetState> _calendarKey =
-      GlobalKey<CalendarWidgetState>();
-  final GlobalKey<GematriaSearchScreenState> _gematriaKey =
-      GlobalKey<GematriaSearchScreenState>();
-  late final List<Widget> _pages;
-  late final List<Widget> _tabWidgets;
+  TabController? _tabController;
+  final GlobalKey<CalendarWidgetState> _calendarKey = GlobalKey<CalendarWidgetState>();
+  final GlobalKey<GematriaSearchScreenState> _gematriaKey = GlobalKey<GematriaSearchScreenState>();
+  
+  List<ToolDescriptor> _descriptors = [];
+  List<Widget> _pages = [];
+  List<Widget> _tabWidgets = [];
+  String? _selectedToolId;
+  bool _isPanelOpen = false;
+  InstalledPlugin? _transientPlugin;
 
   void _requestCalendarFocus({int remainingAttempts = _calendarFocusRetryCount}) {
-    if (!mounted) {
-      return;
-    }
-
+    if (!mounted) return;
     final calendarState = _calendarKey.currentState;
     if (calendarState != null) {
       calendarState.requestKeyboardFocus();
       return;
     }
-
-    if (remainingAttempts <= 0) {
-      return;
-    }
-
+    if (remainingAttempts <= 0) return;
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) {
-        return;
-      }
-
+      if (!mounted) return;
       Future<void>.delayed(const Duration(milliseconds: 50), () {
-        if (mounted) {
-          _requestCalendarFocus(remainingAttempts: remainingAttempts - 1);
-        }
+        if (mounted) _requestCalendarFocus(remainingAttempts: remainingAttempts - 1);
       });
     });
   }
 
-  /// מבקש פוקוס ללשונית הפעילה אם היא לוח השנה.
-  ///
-  /// המתודה מנסה שוב לזמן קצר אם לוח השנה עדיין לא חובר לעץ בזמן
-  /// הקריאה, למשל בזמן אנימציית מעבר בין מסכים.
   void requestActiveTabFocus() {
-    if (_tabController.index == 0) {
+    if (_tabController?.index != null && _descriptors[_tabController!.index].toolId == 'builtin.calendar') {
       _requestCalendarFocus();
     }
   }
 
   void _handleTabChange() {
-    if (_tabController.index == 0) {
-      _requestCalendarFocus();
+    if (_tabController == null) return;
+    final currentToolId = _descriptors[_tabController!.index].toolId;
+    if (currentToolId != _selectedToolId) {
+      setState(() {
+        _selectedToolId = currentToolId;
+      });
+      if (currentToolId == 'builtin.calendar') {
+        _requestCalendarFocus();
+      }
     }
   }
 
-  final List<_TabInfo> _tabs = [
-    const _TabInfo(
-      label: 'לוח שנה',
-      icon: Icons.calendar_month_outlined,
-    ),
-    const _TabInfo(
-      label: 'שמור וזכור',
-      icon: null,
-      imageIcon: 'assets/icon/שמור וזכור שחור ריק.png',
-    ),
-    const _TabInfo(
-      label: 'מדות ושיעורים',
-      icon: Icons.straighten,
-    ),
-    const _TabInfo(
-      label: 'הערות אישיות',
-      icon: FluentIcons.note_24_regular,
-    ),
-    const _TabInfo(
-      label: 'גימטריה',
-      icon: FluentIcons.calculator_24_regular,
-    ),
-    const _TabInfo(
-      label: 'מילון',
-      icon: FluentIcons.book_24_regular,
-    ),
-  ];
+  List<ToolDescriptor> _buildBaseDescriptors() {
+    return [
+      BuiltInToolDescriptor(
+        toolId: 'builtin.calendar',
+        label: 'לוח שנה',
+        icon: Icons.calendar_month_outlined,
+        order: 10,
+        pageBuilder: () => BlocBuilder<CalendarCubit, CalendarState>(
+          builder: (context, _) => CalendarWidget(key: _calendarKey),
+        ),
+      ),
+      BuiltInToolDescriptor(
+        toolId: 'builtin.shamor_zachor',
+        label: 'שמור וזכור',
+        imageIcon: 'assets/icon/שמור וזכור שחור ריק.png',
+        order: 20,
+        pageBuilder: () => ShamorZachorWidget(onTitleChanged: (_) {}),
+      ),
+      BuiltInToolDescriptor(
+        toolId: 'builtin.measurements',
+        label: 'מדות ושיעורים',
+        icon: Icons.straighten,
+        order: 30,
+        pageBuilder: () => const MeasurementConverterScreen(),
+      ),
+      BuiltInToolDescriptor(
+        toolId: 'builtin.notes',
+        label: 'הערות אישיות',
+        icon: FluentIcons.note_24_regular,
+        order: 40,
+        pageBuilder: () => const PersonalNotesManagerScreen(),
+      ),
+      BuiltInToolDescriptor(
+        toolId: 'builtin.gematria',
+        label: 'גימטריה',
+        icon: FluentIcons.calculator_24_regular,
+        order: 50,
+        pageBuilder: () => GematriaSearchScreen(key: _gematriaKey),
+      ),
+      BuiltInToolDescriptor(
+        toolId: 'builtin.dictionary',
+        label: 'מילון',
+        icon: FluentIcons.book_24_regular,
+        order: 60,
+        pageBuilder: () => const DictionaryScreen(),
+      ),
+      if (kDebugMode)
+        BuiltInToolDescriptor(
+          toolId: 'builtin.plugin_test',
+          label: 'Plugin POC (Debug)',
+          icon: FluentIcons.bug_24_regular,
+          order: 999,
+          pageBuilder: () => const PluginTestScreen(),
+        ),
+    ];
+  }
+
+  void _openPluginTransiently(InstalledPlugin plugin) {
+    if (plugin.pinned) {
+      final index = _descriptors.indexWhere((d) => d.toolId == plugin.pluginId);
+      if (index != -1) {
+        _tabController?.animateTo(index);
+      }
+      return;
+    }
+    
+    setState(() {
+      _transientPlugin = plugin;
+      _selectedToolId = plugin.pluginId;
+      final blocState = context.read<PluginSystemBloc>().state;
+      if (blocState is PluginSystemLoaded) {
+        _rebuildTabs(blocState.pinnedPlugins, transient: _transientPlugin);
+      }
+    });
+  }
+
+  void _rebuildTabs(List<InstalledPlugin> pinnedPlugins, {InstalledPlugin? transient}) {
+    if (!mounted) return;
+
+    final newDescriptors = <ToolDescriptor>[
+      ..._buildBaseDescriptors(),
+      ...pinnedPlugins.map((p) => PluginToolDescriptor(plugin: p)),
+    ];
+    if (transient != null) {
+      if (!pinnedPlugins.any((p) => p.pluginId == transient.pluginId)) {
+        newDescriptors.add(PluginToolDescriptor(plugin: transient, isTransient: true));
+      }
+    }
+    
+    newDescriptors.sort((a, b) => a.order.compareTo(b.order));
+
+    int newIndex = 0;
+    if (_selectedToolId != null) {
+      newIndex = newDescriptors.indexWhere((t) => t.toolId == _selectedToolId);
+      if (newIndex == -1) newIndex = 0;
+    }
+    
+    _selectedToolId = newDescriptors[newIndex].toolId;
+
+    final newController = TabController(length: newDescriptors.length, initialIndex: newIndex, vsync: this);
+    newController.addListener(_handleTabChange);
+
+    final oldController = _tabController;
+    _tabController = newController;
+    
+    // We update state inside post frame to avoid triggering updates while building
+    setState(() {
+      _descriptors = newDescriptors;
+      _tabWidgets = newDescriptors.map((t) => t.buildTab(context)).toList();
+      _pages = newDescriptors.map((t) => t.buildPage(context)).toList();
+    });
+
+    if (oldController != null) {
+      oldController.removeListener(_handleTabChange);
+      oldController.dispose();
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-
-    _tabController = TabController(length: _tabs.length, vsync: this);
-    _tabController.addListener(_handleTabChange);
-
-    _tabWidgets = _tabs.map((tab) {
-      if (tab.imageIcon != null) {
-        return SizedBox(
-          width: 100,
-          child: Tab(
-            text: tab.label,
-            icon: ImageIcon(AssetImage(tab.imageIcon!), size: 20),
-          ),
-        );
-      }
-      return SizedBox(
-        width: 100,
-        child: Tab(
-          text: tab.label,
-          icon: Icon(tab.icon, size: 20),
-        ),
-      );
-    }).toList();
-
-    // יצירת הדפים פעם אחת ב-initState
-    _pages = [
-      BlocBuilder<CalendarCubit, CalendarState>(
-        builder: (context, _) => CalendarWidget(key: _calendarKey),
-      ),
-      ShamorZachorWidget(
-        onTitleChanged: (_) {},
-      ),
-      const MeasurementConverterScreen(),
-      const PersonalNotesManagerScreen(),
-      GematriaSearchScreen(key: _gematriaKey),
-      const DictionaryScreen(),
-    ];
+    // Default setup
+    _rebuildTabs([]);
   }
 
-  /// מחזיר את מסך "עוד" ללשונית לוח השנה.
-  ///
-  /// אם לשונית לוח השנה כבר פעילה, מתבצעת בקשת פוקוס מחודשת כדי
-  /// לאפשר ניווט מיידי עם מקשי החיצים.
   void resetToCalendar() {
-    if (_tabController.index != 0) {
-      _tabController.animateTo(0);
+    final calendarIndex = _descriptors.indexWhere((t) => t.toolId == 'builtin.calendar');
+    if (calendarIndex != -1 && _tabController?.index != calendarIndex) {
+      _tabController?.animateTo(calendarIndex);
       return;
     }
-
     _requestCalendarFocus();
   }
 
   @override
   void dispose() {
-    _tabController.removeListener(_handleTabChange);
-    _tabController.dispose();
+    _tabController?.removeListener(_handleTabChange);
+    _tabController?.dispose();
     super.dispose();
   }
 
@@ -172,51 +294,82 @@ class MoreScreenState extends State<MoreScreen>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    final isMoreScreenActive =
-        context.select((NavigationBloc bloc) => bloc.state.currentScreen) ==
-            Screen.more;
+    final isMoreScreenActive = context.select((NavigationBloc bloc) => bloc.state.currentScreen) == Screen.more;
 
-    if (isMoreScreenActive && _tabController.index == 0) {
+    if (isMoreScreenActive && _selectedToolId == 'builtin.calendar') {
       WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (mounted) {
-          requestActiveTabFocus();
-        }
+        if (mounted) requestActiveTabFocus();
       });
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 72,
-        title: TabBar(
-          controller: _tabController,
-          isScrollable: true,
-          tabAlignment: TabAlignment.center,
-          tabs: _tabWidgets,
-        ),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(1.0),
-          child: Container(
-            color: Theme.of(context).dividerColor,
-            height: 1.0,
+    return BlocListener<PluginSystemBloc, PluginSystemState>(
+      listener: (context, state) {
+        if (state is PluginSystemLoaded) {
+          _rebuildTabs(state.pinnedPlugins, transient: _transientPlugin);
+        } else if (state is PluginSystemOverwriteRequired) {
+          showWarningDialog(
+            context: context,
+            title: 'התוסף כבר קיים',
+            content: 'התוסף "${state.pluginName}" בגרסה ${state.version} כבר מותקן.',
+            subtitle: 'האם ברצונך להתקין מחדש ולדרוס אותו?',
+            cancelText: 'ביטול',
+            confirmText: 'התקן מחדש',
+          ).then((value) {
+            if (!context.mounted) return;
+            if (value == true) {
+              context.read<PluginSystemBloc>().add(
+                InstallPluginRequested(state.archivePath, forceOverwrite: true),
+              );
+            } else {
+              context.read<PluginSystemBloc>().add(LoadPlugins());
+            }
+          });
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 72,
+          leading: IconButton(
+            icon: const Icon(FluentIcons.puzzle_piece_24_regular),
+            onPressed: () {
+              setState(() {
+                _isPanelOpen = !_isPanelOpen;
+              });
+            },
+            tooltip: 'תוספים',
+          ),
+          title: _tabController == null ? null : TabBar(
+            controller: _tabController,
+            isScrollable: true,
+            tabAlignment: TabAlignment.center,
+            tabs: _tabWidgets,
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1.0),
+            child: Container(
+              color: Theme.of(context).dividerColor,
+              height: 1.0,
+            ),
           ),
         ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: _pages,
+        body: _tabController == null ? const Center(child: CircularProgressIndicator()) : Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            if (_isPanelOpen) PluginSidePanel(
+              onPluginSelected: (plugin) {
+                _openPluginTransiently(plugin);
+              },
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                key: ValueKey(_descriptors.length), // Rebuild when count changes
+                children: _pages,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
-
-class _TabInfo {
-  final String label;
-  final IconData? icon;
-  final String? imageIcon;
-
-  const _TabInfo({
-    required this.label,
-    this.icon,
-    this.imageIcon,
-  });
 }
